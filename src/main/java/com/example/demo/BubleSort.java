f22be497fdf1d2eba8e393c171938635cbf04a98
lifuchun <01118311@wisedu.com>
2023-09-13 13:52:42 -0400

    bubble sort

---
+++ src/main/java/com/example/demo/BubleSort.java
@@ -0,0 +1,34 @@
+package main.java.com.example.demo;
+
+public class BubleSort {
+    void sort(int arr[])
+    {
+    for(int i=0;i<arr.length-1;i++){
+        for(int j=0;j<arr.length-i-1;j++){
+            if(arr[j+1]<arr[j]){
+              swap(arr,j,j+1);
+            }
+        }
+    }
+    }
+    static void printArray(int arr[])
+    {
+        for (int i = 0; i < arr.length; ++i)
+            System.out.print(arr[i] + " ");
+    }
+
+    public static void main(String args[])
+    {
+        int arr[] = {3, 11, 1,4,5,2,7};
+
+        BubleSort ob = new BubleSort();
+        ob.sort(arr);
+        printArray(arr);
+    }
+
+    private static void swap(int[] arry, int i, int smallIndex) {
+        int temp=arry[i];
+        arry[i]=arry[smallIndex];
+        arry[smallIndex]=temp;
+    }
+}
