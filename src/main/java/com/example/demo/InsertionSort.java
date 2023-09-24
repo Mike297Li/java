8f7375c3bb573f71851751e3c6452c4f0de04f86
lifuchun <01118311@wisedu.com>
2023-09-24 16:20:04 -0400

    InsertionSort

--- src/main/java/com/example/demo/InsertionSort.java
+++ src/main/java/com/example/demo/InsertionSort.java
@@ -2,15 +2,15 @@ package main.java.com.example.demo;
 public class InsertionSort {
     void sort(int arr[])
     {
-       for(int a=0;a<arr.length;a++){
-           int currentVal=arr[a];
-           int b=a-1;
-           while(b>=0 && arr[b]>currentVal){
-               arr[b+1]=arr[b];
-               b--;
-           }
-           arr[b+1]=currentVal;
-       }
+        for(int pointer=1;pointer<arr.length;pointer++){
+            int currentVal=arr[pointer];
+            int postion=pointer;
+            while(postion>0 && arr[postion-1]>currentVal){
+                arr[postion]=arr[postion-1];
+                postion--;
+            }
+            arr[postion]=currentVal;
+        }
     }
     static void printArray(int arr[])
     {
@@ -20,7 +20,7 @@ public class InsertionSort {
 
     public static void main(String args[])
     {
-        int arr[] = {3, 11, 1, 5, 6 };
+        int arr[] = {3, 2,8,11, 1, 5, 6 };
 
         InsertionSort ob = new InsertionSort();
         ob.sort(arr);
