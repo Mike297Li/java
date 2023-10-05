1efa6aeb5fa322a700409e9e3a91cd0277354f8f
Mike <fuchun297@gmail.com>
2023-10-04 20:27:00 -0400

    REWRITE

--- src/main/java/com/example/demo/InsertionSort.java
+++ src/main/java/com/example/demo/InsertionSort.java
@@ -2,15 +2,15 @@ package main.java.com.example.demo;
 public class InsertionSort {
     void sort(int arr[])
     {
-      for(int pointer=1;pointer<arr.length;pointer++){
-          int position=pointer;
-          int currentVal=arr[pointer];
-          while (position>0 && arr[position-1]>currentVal) {
-              arr[position]=arr[position-1];
-              position--;
-          }
-          arr[position]=currentVal;
-      }
+     for(int pointer=1;pointer<arr.length;pointer++){
+         int position=pointer;
+         int currentVal=arr[pointer];
+         while(position>0 && arr[position-1]>currentVal){
+             arr[position]=arr[position-1];
+             position--;
+         }
+         arr[position]=currentVal;
+     }
     }
     static void printArray(int arr[])
     {
